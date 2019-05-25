0fda834dcebb30153109782c76e44a64caeedfee
Nikki Quibin <nikkiquibin@Nikkis-MacBook-Pro.local>
2019-05-25 17:50:00 -0400

    added aos

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
+/* global AOS */
+
 import '../styles/main.scss';
+
+const addAOSAttributes = () => {
+    $('.exp-content').each((index, value) => {
+        const isEven = (index + 1) % 2 === 0;
+        $(value).attr('data-aos', `zoom-in-${isEven ? 'left' : 'right'}`);
+    });
+
+	$('.project-content').each((index, value) => {
+		const isEven = (index + 1) % 2 === 0;
+		$(value).attr('data-aos', `zoom-in-${isEven ? 'left' : 'right'}`);
+	});
+};
+
+addAOSAttributes();
+
+$(() => {
+    AOS.init({ once: true });
+});
