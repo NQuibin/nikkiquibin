92e822c74a52b9ab885ae54614989ce49e3c6a95
Nikki Quibin <nikkiquibin@Nikkis-MacBook-Pro.local>
2019-05-26 11:50:26 -0400

    changed styles

--- src/js/app.js
+++ src/js/app.js
@@ -5,12 +5,12 @@ import '../styles/main.scss';
 const addAOSAttributes = () => {
     $('.exp-content').each((index, value) => {
         const isEven = (index + 1) % 2 === 0;
-        $(value).attr('data-aos', `zoom-in-${isEven ? 'left' : 'right'}`);
+        $(value).attr('data-aos', `fade-${isEven ? 'down' : 'up'}`);
     });
 
 	$('.project-content').each((index, value) => {
 		const isEven = (index + 1) % 2 === 0;
-		$(value).attr('data-aos', `zoom-in-${isEven ? 'left' : 'right'}`);
+		$(value).attr('data-aos', `fade-${isEven ? 'down' : 'up'}`);
 	});
 };
 
