006a9d75c0a000f44a89eadc16171b57d1d24470
Nikki Quibin <nikkiquibin@Nikkis-MacBook-Pro.local>
2020-02-06 21:42:40 -0500

    initial build subtree commit

---
+++ precache-manifest.97964e8709b7f6f989b596ba66c4a6df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4a23305f99cd84e67ce9294d74c7a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f951c8c80b528ac198e4",
+    "url": "/static/js/2.1113e4a5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.1113e4a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a350b98ccb37408ba50f",
+    "url": "/static/js/main.83749773.chunk.js"
+  },
+  {
+    "revision": "661014e973f415c204e7",
+    "url": "/static/js/runtime-main.f805b5b9.js"
+  },
+  {
+    "revision": "9b9b3343906998e2a5f0ad274a0855b5",
+    "url": "/static/media/headshot.9b9b3343.jpg"
+  },
+  {
+    "revision": "5c86c8ba43beb988c33ec3ea6dbb0911",
+    "url": "/static/media/nikki_quibin_resume_2020-02-06.5c86c8ba.pdf"
+  }
+]);
